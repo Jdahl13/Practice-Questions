1b9a0ff47ec47bed21182e46f9029c5ee5c13bbb
jdahl13 <jdahl07@gmail.com>
2024-02-28 20:33:12 -0800

    Added comments

--- HR_Grading_Students.py
+++ HR_Grading_Students.py
@@ -5,28 +5,29 @@ import random
 #are less than 3 points away from the next multiple of 5 of that num (83 ->85, etc). If the grade is less than 50
 # there is no rounding as this is a fail. grades must be 0-100 
 
-grades = []
-for grade in range(6): 
+grades = []                                 # blank grade book
+for grade in range(6):                      #fill with grades
     grades.append(random.randint(40,100))
 def adjustGrades(gradeList):
     'takes list of grades and determines if rounding is appropriate'
-    updatedGrades = []
+    updatedGrades = []                      # new list to hold grades after rounding is determined
     for grade in gradeList:
         old = grade
         updated = 0
         new = 0
-        if grade < 40:
+        if grade < 40:                      # auto failure, no need to check if it can be rounded
             updatedGrades.append(grade)
-        updated = old - (old % 5) + 5
+        updated = old - (old % 5) + 5       # find the next multiple of 5 after number
         new = updated - old
-        if new < 3:
+        if new < 3:                         #determine if meets rounding criteria
             updatedGrades.append(updated)
         else:
             updatedGrades.append(grade)
     return updatedGrades
-print(grades)
-finalGrades = adjustGrades(grades)
-print(finalGrades)
+print(grades)                               #before gradebook
+finalGrades = adjustGrades(grades)          
+print(finalGrades)                          #after function is run 
     
         
-#print(grades)
+#This seems to solve the question! Furture things to add, checking if gradebook has values in range
+#can probably optimize this to reduce code
