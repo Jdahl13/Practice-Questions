f645bc5148ea0b5bd8ccd1b25237c5df6d766472
jdahl13 <jdahl07@gmail.com>
2024-02-28 20:28:42 -0800

    added fucntion that adjusts grades

--- HR_Grading_Students.py
+++ HR_Grading_Students.py
@@ -8,5 +8,25 @@ import random
 grades = []
 for grade in range(6): 
     grades.append(random.randint(40,100))
-
+def adjustGrades(gradeList):
+    'takes list of grades and determines if rounding is appropriate'
+    updatedGrades = []
+    for grade in gradeList:
+        old = grade
+        updated = 0
+        new = 0
+        if grade < 40:
+            updatedGrades.append(grade)
+        updated = old - (old % 5) + 5
+        new = updated - old
+        if new < 3:
+            updatedGrades.append(updated)
+        else:
+            updatedGrades.append(grade)
+    return updatedGrades
 print(grades)
+finalGrades = adjustGrades(grades)
+print(finalGrades)
+    
+        
+#print(grades)
