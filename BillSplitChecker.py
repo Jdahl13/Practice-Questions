020de1619b104bfb767d87b31be3c2958a88b2d3
jdahl13 <jdahl07@gmail.com>
2024-03-04 16:09:15 -0800

    Solution works, but needs major refactor

--- BillSplitChecker.py
+++ BillSplitChecker.py
@@ -1,15 +1,18 @@
-#Bill and Anna spit their check. Anna will only pay for items she ordered. Function checks to see if Bill's cal
+#Bill and Anna spit their check. Anna will only pay for items she ordered. Function checks to see if Bill's calc
 #is correct or not. Input provided will be a list that reps the "Bill", a number that includes what anna didnt 
 #eat, and Bills calculation. If the calc matches the true split, print "Correct"
 import random
 
-def billChecker(bill,annasSubtractions,billsCalc):
-    'take 3 inputs to check if Bills calc for split is correct'
+def billChecker(bill, annasBill, billsEstSplit):
+    'checks to see if Bill is correctly calculating the bill split'
+    x = billsEstSplit
+    y = annasBill
+    if x == y:
+        print("Bill's suggested split is correct!")
+    else:
+        print("Bill's wants Anna to pay her unfair share")
     
     
-    
-    
-
 #Set up bill
 bill = []
 sizeOfBill= random.randint(1,10)
@@ -19,12 +22,42 @@ for item in range(0,sizeOfBill):
 
 
 #set up anna and if she will have anything to remove
+annasIndexToRemove = 0
 x = random.randint(1,2)
 if x == 1:
-    print("Anna splits everthing on the bill")
+    print("Anna does not split everything on the bill")
+    annasIndexToRemove = random.randint(1,len(bill)-1)
+else:
+    print("Anna splits everything on the bill")
+
+#generate Bill's est split
+split = 0
+for item in range(len(bill)):
+    item = bill[item]
+    split += item
+split = split/2
+print(split)
+
+#generate anna's split if different
+annaSplit = 0
+if annasIndexToRemove > 0:
+    bill.remove(bill[annasIndexToRemove])
+for item in range(len(bill)):
+    item = bill[item]
+    annaSplit += item
+annaSplit = annaSplit / 2
+
+billChecker(bill, annaSplit, split)
+
+
+
+
+
 
 
 #driver code:
-print(bill) 
+
+
+
 
 
